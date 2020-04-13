144a97a9c2224379246d4aa82c4ab40e85984c38
Olivier Bohrer <olivier.bohrer@gmail.com>
2020-04-13 19:33:30 +0100

    release v0.1.2: bump maven-shade-plugin + amend clojars url

--- project.clj
+++ project.clj
@@ -4,6 +4,6 @@
   :license {:name "Eclipse Public License" :url "http://www.eclipse.org/legal/epl-v20.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.apache.maven.plugins/maven-shade-plugin "3.2.2"]]
-  :deploy-repositories [["releases"  {:sign-releases false :url "https://clojars.org"}]
-                        ["snapshots" {:sign-releases false :url "https://clojars.org"}]]
+  :deploy-repositories [["releases"  {:sign-releases false :url "https://repo.clojars.org"}]
+                        ["snapshots" {:sign-releases false :url "https://repo.clojars.org"}]]
   :eval-in-leiningen true)
